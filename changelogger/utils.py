47ff78f119625f00638b4a6d22d74453ba624e9d
Austin Ward <ward.austin28@gmail.com>
2023-02-02 23:50:19 -0500

    Cleanup utils

--- changelogger/utils.py
+++ changelogger/utils.py
@@ -1,11 +1,19 @@
+from pathlib import Path
 import re
 from functools import cache
-from typing import IO, Any, Generator, Pattern
+from typing import Generator, Pattern,  TextIO
 from contextlib import contextmanager
 from os import getcwd
 
-from pydantic import FilePath
 from git.repo import Repo
+
+
+KEY_URL = "url"
+GITHUB_HTTPS_PREFIX = "https://github.com/"
+GITHUB_SSH_PREFIX = "git@github.com:"
+GIT_SUFFIX = ".git"
+
+MODE_READ_AND_WRITE = "r+"
 
 
 @cache
@@ -14,17 +22,19 @@ def cached_compile(pattern: str) -> Pattern:
 
 
 @contextmanager
-def open_rw(filename: str | FilePath) -> Generator[tuple[IO[Any], str], None, None]:
-    with open(filename, "r+") as f:
+def open_rw(filename: str | Path) -> Generator[tuple[TextIO, str], None, None]:
+    with open(filename, MODE_READ_AND_WRITE) as f:
         content = f.read()
         f.seek(0)
         yield (f, content)
 
 
 def get_git_repo() -> str:
-    git_url = Repo(getcwd()).remotes.origin.config_reader.get("url")
+    git_url = Repo(getcwd()).remotes.origin.config_reader.get(KEY_URL)
     return git_url.lstrip(
-        "https://github.com/",
+        GITHUB_HTTPS_PREFIX,
     ).lstrip(
-        "git@github.com:",
-    ).rstrip(".git")
+        GITHUB_SSH_PREFIX,
+    ).rstrip(
+        GIT_SUFFIX,
+    )
