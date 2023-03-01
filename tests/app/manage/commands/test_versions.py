f9a1b593e9a7040aec4f60ede82532e824582e0e
award28 <austin.ward@klaviyo.com>
2023-03-01 17:38:19 -0500

    Add versions command

---
+++ tests/app/manage/commands/test_versions.py
@@ -0,0 +1,51 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+import semver
+
+from changelogger.app.manage.commands.versions import versions
+
+
+class TestManageVersionsCommand:
+    VERSIONS = [semver.VersionInfo(v) for v in reversed(range(100))]
+
+    @pytest.fixture
+    def mock_changelog(self):
+        with patch(
+            "changelogger.app.manage.commands.versions.changelog"
+        ) as mock:
+            yield mock
+
+    @pytest.fixture
+    def mock_print(self):
+        with patch("changelogger.app.manage.commands.versions.print") as mock:
+            yield mock
+
+    def test_versions(
+        self,
+        mock_changelog: MagicMock,
+        mock_print: MagicMock,
+    ) -> None:
+        mock_changelog.get_all_versions.side_effect = (self.VERSIONS,)
+        versions(latest=False, show_all=False, num_versions=10)
+        mock_print.assert_called_once_with(
+            "\n".join(map(str, self.VERSIONS[:10]))
+        )
+
+    def test_versions_latest(
+        self,
+        mock_changelog: MagicMock,
+        mock_print: MagicMock,
+    ) -> None:
+        mock_changelog.get_latest_version.side_effect = (self.VERSIONS[0],)
+        versions(latest=True, show_all=False, num_versions=10)
+        mock_print.assert_called_once_with(self.VERSIONS[0])
+
+    def test_versions_show_all(
+        self,
+        mock_changelog: MagicMock,
+        mock_print: MagicMock,
+    ) -> None:
+        mock_changelog.get_all_versions.side_effect = (self.VERSIONS,)
+        versions(latest=False, show_all=True, num_versions=10)
+        mock_print.assert_called_once_with("\n".join(map(str, self.VERSIONS)))
