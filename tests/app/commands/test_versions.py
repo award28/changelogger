8cc157b5e51c18ab4791c6c2cc34553e446c4942
Austin Ward <ward.austin28@gmail.com>
2023-03-04 16:41:30 -0500

    Tests start and offset

--- tests/app/commands/test_versions.py
+++ tests/app/commands/test_versions.py
@@ -47,3 +47,28 @@ class TestManageVersionsCommand:
         mock_changelog.get_all_versions.side_effect = (self.VERSIONS,)
         versions(latest=False, show_all=True, start=0, offset=10)
         mock_print.assert_called_once_with("\n".join(map(str, self.VERSIONS)))
+
+    @pytest.mark.parametrize(
+        "start,offset",
+        [
+            (0, 100),
+            (100, 100),
+            (100, 1000),
+            (1000, 1000),
+            (2, 35),
+            (70, 80),
+            (80, 70),
+        ],
+    )
+    def test_versions_start_and_offset(
+        self,
+        start: int,
+        offset: int,
+        mock_changelog: MagicMock,
+        mock_print: MagicMock,
+    ) -> None:
+        mock_changelog.get_all_versions.side_effect = (self.VERSIONS,)
+        versions(latest=False, show_all=False, start=start, offset=offset)
+        mock_print.assert_called_once_with(
+            "\n".join(list(map(str, self.VERSIONS))[start : start + offset])
+        )
