1c3c970c4fa156545cfb4120e1f74f72dd32ad3a
Austin Ward <ward.austin28@gmail.com>
2023-03-05 22:29:47 -0500

    Add a comment describing whats happening

--- changelogger/templating.py
+++ changelogger/templating.py
@@ -22,15 +22,16 @@ def update(
         assert file.jinja_rel_path, "No valid jinja template found."
         replacement_str = file.jinja_rel_path.read_text()
 
-    variables = _get_variables(file, update)
-    pattern = render_jinja(file.pattern, variables)
-    render = partial(render_jinja, replacement_str)
+    var_getter = partial(_get_variables, file, update)
+    pattern = render_jinja(file.pattern, var_getter())
 
-    def replacer(m: Match) -> str:
-        variables["context"]["match"] = m
-        return render(variables)
-
-    return cached_compile(pattern).sub(replacer, content)
+    # re.sub can take a callable as the replacement argument rather than a
+    # string. This callable accepts a match and returns a string. For each
+    # match which is found, re.sub will call repl with the match, and
+    # replace the found pattern with the output string of the user supplied
+    # repl function.
+    repl = lambda m: render_jinja(replacement_str, var_getter(m))
+    return cached_compile(pattern).sub(repl, content)
 
 
 def render_pattern(
@@ -53,6 +54,7 @@ def _tmpl(jinja: str) -> Template:
 def _get_variables(
     versioned_file: VersionedFile,
     update: ChangelogUpdate,
+    match: Match | None = None,
 ) -> dict[str, Any]:
     return dict(
         new_version=update.new_version,
@@ -60,4 +62,5 @@ def _get_variables(
         today=date.today(),
         sections=update.release_notes.dict(),
         context=versioned_file.context,
+        match=match,
     )
